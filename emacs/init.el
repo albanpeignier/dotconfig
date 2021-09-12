39d700c2680438ed4317edb32d5e1e6fe12a8393
Alban Peignier <alban@hyppo.tryphon.priv>
2021-09-12 09:35:59 +0200

    Add markdown-mode emacs package

--- emacs/init.el
+++ emacs/init.el
@@ -10,7 +10,7 @@
   (package-refresh-contents))
 
 (setq package-list
-      '(yafolding ace-window yasnippet-classic-snippets flymake-go flymake-ruby coffee-mode f flx ghub git-gutter inf-ruby inflections json-snatcher magit-popup ov robe rspec-mode string-inflection treepy yaml-mode async projectile projectile-rails feature-mode copy-as-format csv-mode itail rainbow-mode abyss-theme dockerfile-mode magit bundler coverage duplicate-thing json-reformat json-mode twilight-theme go-mode yasnippet flx-ido haml-mode slim-mode scss-mode))
+      '(yafolding ace-window yasnippet-classic-snippets flymake-go flymake-ruby coffee-mode f flx ghub git-gutter inf-ruby inflections json-snatcher magit-popup ov robe rspec-mode string-inflection treepy yaml-mode async projectile projectile-rails feature-mode copy-as-format csv-mode itail rainbow-mode abyss-theme dockerfile-mode magit bundler coverage duplicate-thing json-reformat json-mode twilight-theme go-mode yasnippet flx-ido haml-mode slim-mode scss-mode markdown-mode))
 
 ; install the missing packages
 (dolist (package package-list)
@@ -29,7 +29,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(yasnippet-classic-snippets yaml-mode yafolding twilight-theme string-inflection slim-mode scss-mode rspec-mode robe rainbow-mode projectile-rails magit-popup magit json-mode itail haml-mode go-mode git-gutter ghub flymake-ruby flymake-go flx-ido feature-mode duplicate-thing dockerfile-mode csv-mode coverage copy-as-format coffee-mode bundler async ace-window abyss-theme))
+   '(markdown-mode yasnippet-classic-snippets yaml-mode yafolding twilight-theme string-inflection slim-mode scss-mode rspec-mode robe rainbow-mode projectile-rails magit-popup magit json-mode itail haml-mode go-mode git-gutter ghub flymake-ruby flymake-go flx-ido feature-mode duplicate-thing dockerfile-mode csv-mode coverage copy-as-format coffee-mode bundler async ace-window abyss-theme))
  '(tab-width 2)
  '(tramp-default-method "ssh" t))
 (custom-set-faces

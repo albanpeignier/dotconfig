fd8cce474e09ca8f60d67d969a1cb022e574099d
Alban Peignier <alban@tryphon.org>
2023-10-03 18:17:54 +0200

    Use code as EDITOR

---
+++ oh-my-zsh/custom/editor.zsh
@@ -0,0 +1,4 @@
+# export EDITOR="emacsclient.emacs-snapshot"
+# alias e="~/.bin/emacs-open"
+
+export EDITOR="code --wait"
--- oh-my-zsh/custom/emacs.zsh
+++
@@ -1,2 +0,0 @@
-export EDITOR="emacsclient.emacs-snapshot"
-alias e="~/.bin/emacs-open"

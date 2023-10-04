50ccd57a3dabb1808c6ecfaf20ded334a4eaa3e8
Alban Peignier <alban@tryphon.org>
2023-10-04 18:06:12 +0200

    Add cucumber feature-mode emacs package

--- doom-emacs/packages.el
+++ doom-emacs/packages.el
@@ -9,6 +9,9 @@
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 
+; Emacs mode for editing Cucumber plain text user stories: https://github.com/michaelklishin/cucumber.el
+(package! feature-mode)
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/radian-software/straight.el#the-recipe-format
